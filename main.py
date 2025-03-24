f9192cd85145a95a84693bc0a820698716835a48
Shahid Ul Islam <65996596+Khanz9664@users.noreply.github.com>
2025-03-24 20:32:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,4 +55,5 @@ def scrape():
         return jsonify(scraped_data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)  # Updated host and port
+
